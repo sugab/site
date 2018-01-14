5dcbf4a72e311ebc13e7ee6ec14c6cfae6058293
Bagus Cahyono <baguscah77@gmail.com>
2018-01-14 11:13:34 +0800

    awesome social links

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,6 +6,26 @@
     <div class="text">
       <span class="subtitle boxed">Fullstack Developer, Data Geek, and Traveller.</span>
     </div>
+    <div class="social-links">
+      <span class="boxed">
+        <a class="social-link" href="https://github.com/sugab" target="blank">
+          <i class="fab fa-github"></i>
+          <b>github</b>
+        </a>
+      </span>
+      <span class="boxed">
+        <a class="social-link" href="https://stackoverflow.com/users/903350/bagus-cahyono" target="blank">
+          <i class="fab fa-stack-overflow"></i>
+          <b>stackoverflow</b>
+        </a>
+      </span>
+      <span class="boxed">
+        <a class="social-link" href="https://www.linkedin.com/in/cahyonobagus" target="blank">
+          <i class="fab fa-linkedin"></i>
+          <b>linkedin</b>
+        </a>
+      </span>
+    </div>
   </div>
 </template>
 
@@ -22,6 +42,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+a {
+  text-decoration: none;
+  color: inherit;
+}
+
 .container {
   padding: 0 20px;
   margin-top: 10%;
@@ -43,5 +68,24 @@ export default {
 
 .subtitle {
   font-size: 1.2em;
+}
+
+.social-links {
+  margin-top: 20px;
+}
+
+.social-link {
+  padding-left: 4px;
+}
+
+.social-link > b {
+  display: inline-block;
+  max-width: 0px;
+  overflow: hidden;
+  transition: all 1s ease;
+}
+
+.social-link:hover > b {
+  max-width: 200px;
 }
 </style>
