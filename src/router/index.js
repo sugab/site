81d0159a62596f4d166f533cf33d4bfb0caa3a70
Bagus Cahyono <baguscah77@gmail.com>
2018-01-15 17:03:15 +0800

    redirect root path to home path

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,23 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 const routerOptions = [
-  { path: '/', component: 'Home' },
+  { path: '/', redirect: '/home' },
+  { path: '/home', component: 'Home' },
   { path: '/resume', component: 'Resume' },
   { path: '/photos', component: 'Photos' }
 ]
 
 const routes = routerOptions.map(route => {
-  return {
-    path: route.path,
-    component: () => import(`@/components/${route.component}`)
+  if (route.component) {
+    return {
+      path: route.path,
+      component: () => import(`@/components/${route.component}`)
+    }
+  } else {
+    return {
+      path: route.path,
+      redirect: route.redirect
+    }
   }
 })
 
