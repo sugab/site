d83bbbec83ce849b4d3864d55f060b120ce7c595
Bagus Cahyono <baguscah77@gmail.com>
2018-01-15 16:39:32 +0800

    i think this is the better format :/

--- src/App.vue
+++ src/App.vue
@@ -13,86 +13,77 @@
 </template>
 
 <script>
-  export default {
-    name: 'app',
-    data () {
-      return {
-        menuItems: [{
-          path: '/',
-          title: 'Home'
-        },
-        {
-          path: '/resume',
-          title: 'Resume'
-        },
-        {
-          path: '/photos',
-          title: 'Photos'
-        }
-        ]
-      }
+export default {
+  name: 'app',
+  data () {
+    return {
+      menuItems: [
+        { path: '/', title: 'Home' },
+        { path: '/resume', title: 'Resume' },
+        { path: '/photos', title: 'Photos' }
+      ]
     }
   }
+}
 </script>
 
 <style>
-  * {
-    margin: 0;
-    padding: 0;
-    border: 0;
-    outline: 0;
-    font-size: 100%;
-    vertical-align: baseline;
-    background: transparent;
-  }
+* {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  outline: 0;
+  font-size: 100%;
+  vertical-align: baseline;
+  background: transparent;
+}
 
-  body,
-  html {
-    height: 100%;
-    margin: 0;
-  }
+body,
+html {
+  height: 100%;
+  margin: 0;
+}
 
-  a {
-    text-decoration: none;
-    color: inherit;
-  }
+a {
+  text-decoration: none;
+  color: inherit;
+}
 
-  #app {
-    font-family: "Avenir", Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #2c3e50;
-    min-height: 640px;
-  }
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  min-height: 640px;
+}
 
-  .bg {
-    background-image: url("./assets/images/bg-1.jpg");
-    height: 100%;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-  }
+.bg {
+  background-image: url("./assets/images/bg-1.jpg");
+  height: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
 
-  .header {
-    height: 80px;
-    background: linear-gradient(to top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8));
-  }
+.header {
+  height: 80px;
+  background: linear-gradient(to top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8));
+}
 
-  .menu {
-    margin: 0 5%;
-    padding: 0 20px;
-    float: right;
-  }
+.menu {
+  margin: 0 5%;
+  padding: 0 20px;
+  float: right;
+}
 
-  .menu li {
-    display: inline-block;
-    list-style: none;
-    padding: 10px 20px;
-    color: white;
-  }
+.menu li {
+  display: inline-block;
+  list-style: none;
+  padding: 10px 20px;
+  color: white;
+}
 
-  .menu li.active {
-    font-weight: bold;
-  }
-
+.menu li.active {
+  font-weight: bold;
+}
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -46,99 +46,96 @@
 </template>
 
 <script>
-  import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
-  import {
-    faGithub,
-    faStackOverflow,
-    faLinkedin
-  } from '@fortawesome/fontawesome-free-brands'
-  import {
-    faEnvelope
-  } from '@fortawesome/fontawesome-free-solid'
+import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
+import {
+  faGithub,
+  faStackOverflow,
+  faLinkedin
+} from '@fortawesome/fontawesome-free-brands'
+import { faEnvelope } from '@fortawesome/fontawesome-free-solid'
 
-  export default {
-    name: 'Home',
-    data () {
-      return {
-        name: 'Bagus Cahyono'
-      }
-    },
-    computed: {
-      faGithub () {
-        return faGithub
-      },
-      faStackOverflow () {
-        return faStackOverflow
-      },
-      faLinkedin () {
-        return faLinkedin
-      },
-      faEnvelope () {
-        return faEnvelope
-      }
-    },
-    components: {
-      FontAwesomeIcon
+export default {
+  name: 'Home',
+  data () {
+    return {
+      name: 'Bagus Cahyono'
     }
+  },
+  computed: {
+    faGithub () {
+      return faGithub
+    },
+    faStackOverflow () {
+      return faStackOverflow
+    },
+    faLinkedin () {
+      return faLinkedin
+    },
+    faEnvelope () {
+      return faEnvelope
+    }
+  },
+  components: {
+    FontAwesomeIcon
   }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .content-container {
-    padding: 0 20px;
-    margin: 0 5%;
-    margin-top: 5%;
-    text-align: right;
-  }
+.content-container {
+  padding: 0 20px;
+  margin: 0 5%;
+  margin-top: 5%;
+  text-align: right;
+}
 
-  .boxed {
-    display: inline-block;
-    background-color: rgba(236, 240, 241, 0.8);
-    padding: 5px 10px;
-    margin: 2px;
-  }
+.boxed {
+  display: inline-block;
+  background-color: rgba(236, 240, 241, 0.8);
+  padding: 5px 10px;
+  margin: 2px;
+}
 
+.long-text {
+  max-width: 50%;
+  margin: 40px 0;
+  font-size: 1.2em;
+}
+
+.long-text p {
+  margin: 0;
+}
+
+.name {
+  font-weight: bold;
+  font-size: 3em;
+}
+
+.subtitle {
+  font-size: 1.2em;
+}
+
+.social-link {
+  padding-left: 4px;
+}
+
+.social-link > b {
+  display: inline-block;
+  max-width: 0px;
+  overflow: hidden;
+  transition: all 1s ease;
+}
+
+.social-link:hover > b {
+  max-width: 200px;
+}
+
+@media (max-width: 540px) {
   .long-text {
-    max-width: 50%;
+    max-width: 100%;
     margin: 40px 0;
     font-size: 1.2em;
   }
-
-  .long-text p {
-    margin: 0;
-  }
-
-  .name {
-    font-weight: bold;
-    font-size: 3em;
-  }
-
-  .subtitle {
-    font-size: 1.2em;
-  }
-
-  .social-link {
-    padding-left: 4px;
-  }
-
-  .social-link>b {
-    display: inline-block;
-    max-width: 0px;
-    overflow: hidden;
-    transition: all 1s ease;
-  }
-
-  .social-link:hover>b {
-    max-width: 200px;
-  }
-
-  @media (max-width: 540px) {
-    .long-text {
-      max-width: 100%;
-      margin: 40px 0;
-      font-size: 1.2em;
-    }
-  }
-
+}
 </style>
