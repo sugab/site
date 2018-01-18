42eced86f96fbe0773c82ab060b416c44265fb18
Bagus Cahyono <baguscah77@gmail.com>
2018-01-18 23:06:27 +0800

    cleaner resume

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,110 +1,94 @@
 <template>
   <transition enter-active-class="animated fadeInRight" leave-active-class="animated fadeOutLeft">
     <div class="content-wrapper">
-      <transition enter-active-class="animated fadeInRight" leave-active-class="animated fadeOutLeft">
-        <div v-if="shows[0]">
-          <div class="row">
-            <div class="col-md-12">
-              <span class="boxed boxed-inline section-title">Work Experience</span>
+      <div class="row">
+        <div class="col-md-2 col-md-offset-1">
+          <span class="section-title">Work Experience</span>
+        </div>
+        <div class="col-md-6">
+          <span class="float-right">
+            <img class="company-logo" src="../assets/images/resume/eaciit-logo.png" alt="">
+          </span>
+          <h1>EACIIT Vyasa Pte. Ltd.</h1>
+          <h2>Senior Software Engineer</h2>
+        </div>
+        <div class="col-md-3">
+          <div class="right-content">
+            <div>
+              <b>Singapore</b>
             </div>
+            <div>Mar 2017 - Now</div>
           </div>
         </div>
-      </transition>  
-      <transition enter-active-class="animated fadeInRight" leave-active-class="animated fadeOutLeft">
-        <div v-if="shows[1]">
-          <div class="container-fluid boxed">
-            <div class="row">
-              <div class="col-md-12">
-                <img class="company-logo" src="../assets/images/resume/eaciit-logo.png" alt="">
-                <div class="float-right">
-                  <div>
-                    <b>Singapore</b>
-                  </div>
-                  <div>Mar 2017 - Now</div>
-                </div>
-                <h2>EACIIT Vyasa Pte. Ltd.</h2>
-                <h3>Senior Software Engineer</h3>
-              </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+          <ul class="dashed">
+            <li>Lead development team to complete the software development requirements in a limited time frame and worked closely with Business Analyst and SME to ensure correct business logic, interactive UI, delivered the project with a short turn around</li>
+            <li>Imported reports into platform and converted the same into raw data</li>
+            <li>Wrote extensive test cases to ensure that the application is ready before the go-live date</li>
+            <li>Was the single point of contact from a development perspective for all ad-hoc queries received from the client</li>
+            <li>Imported Data from client’s raw data sources into platform and perform logical operation or query to summarize this data so it can be visualized to support decision making</li>
+            <li>Built Machine Learning libraries based in Golang</li>
+            <li>Worked with the team on Big Data projects using Go and MongoDB for various clients</li>
+            <li>Convert business document into technical detail which is distributed among developers and handle SLA management to complete task</li>
+          </ul>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+          <span class="float-right">
+            <img class="company-logo" src="../assets/images/resume/bilinedev-logo.png" alt="">
+          </span>
+          <h1>BilineDev</h1>
+          <h2>iOS Programmer</h2>
+        </div>
+        <div class="col-md-3">
+          <div class="right-content">
+            <div>
+              <b>Bandung, Indonesia</b>
             </div>
-            <div class="row">
-              <div class="col-md-6 card-content">
-                <ul class="dashed">
-                  <li>Lead development team to complete the software development requirements in a limited time frame and worked closely with Business Analyst and SME to ensure correct business logic, interactive UI, delivered the project with a short turn around</li>
-                  <li>Imported reports into platform and converted the same into raw data</li>
-                  <li>Wrote extensive test cases to ensure that the application is ready before the go-live date</li>
-                  <li>Was the single point of contact from a development perspective for all ad-hoc queries received from the client</li>
-                </ul>
-              </div>
-              <div class="col-md-6 card-content">
-                <ul class="dashed">
-                  <li>Imported Data from client’s raw data sources into platform and perform logical operation or query to summarize this data so it can be visualized to support decision making</li>
-                  <li>Built Machine Learning libraries based in Golang</li>
-                  <li>Worked with the team on Big Data projects using Go and MongoDB for various clients</li>
-                  <li>Convert business document into technical detail which is distributed among developers and handle SLA management to complete task</li>
-                </ul>
-              </div>
-            </div>
+            <div>May 2016 - Dec 2016</div>
           </div>
         </div>
-      </transition>
-      <transition enter-active-class="animated fadeInRight" leave-active-class="animated fadeOutLeft">
-        <div v-if="shows[2]">
-          <div class="container-fluid boxed">
-            <div class="row">
-              <div class="col-md-12">
-                <img class="company-logo" src="../assets/images/resume/bilinedev-logo.png" alt="">
-                <div class="float-right">
-                  <div>
-                    <b>Bandung, Indonesia</b>
-                  </div>
-                  <div>May 2016 - Dec 2016</div>
-                </div>
-                <h2>BilineDev</h2>
-                <h3>iOS Programmer</h3>
-              </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+          <ul class="dashed">
+            <li>Responsible for building, handling, and maintenance of all iOS project</li>
+            <li>Worked closely with CTO to analyze requirement into milestone and reasonable timeline</li>
+            <li>Ensure information result validity, system stability, and data correctness to meet client’s expectation</li>
+          </ul>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+          <span class="float-right">
+            <img class="company-logo" src="../assets/images/resume/bc-logo.png" alt="">
+          </span>
+          <h1>Freelance</h1>
+          <h2>iOS Programmer</h2>
+        </div>
+        <div class="col-md-3">
+          <div class="right-content">
+            <div>
+              <b>Anywhere</b>
             </div>
-            <div class="row">
-              <div class="col-md-12 card-content">
-                <ul class="dashed">
-                  <li>Responsible for building, handling, and maintenance of all iOS project</li>
-                  <li>Worked closely with CTO to analyze requirement into milestone and reasonable timeline</li>
-                  <li>Ensure information result validity, system stability, and data correctness to meet client’s expectation</li>
-                </ul>
-              </div>
-            </div>
+            <div>2014 - Now</div>
           </div>
         </div>
-      </transition>  
-      <transition enter-active-class="animated fadeInRight" leave-active-class="animated fadeOutLeft">
-        <div v-if="shows[3]">
-          <div class="container-fluid boxed">
-            <div class="row">
-              <div class="col-md-12">
-                <img class="company-logo" src="../assets/images/resume/bc-logo.png" alt="">
-                <div class="float-right">
-                  <div>
-                    <b>Bandung, Indonesia</b>
-                  </div>
-                  <div>2014 - Now</div>
-                </div>
-                <h2>Freelance</h2>
-                <h3>iOS Programmer</h3>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-12 card-content">
-                <span>Worked on the below Projects:</span>
-                <ul class="dashed">
-                  <li>GIS Application requested by Directorate General of Land Transportation Indonesia </li>
-                  <li>Patient Medical Data Storing Application requested by Medical Doctor Network Indonesia</li>
-                  <li>Harbor Reporting Application requested by Directorate of Traffic and Sea Transportation Indonesia</li>
-                  <li>Transportation Dashboard &amp; Report Application requested by Ministry of Transportation Indonesia</li>
-                </ul>
-              </div>
-            </div>
-          </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+          <span>Worked on the below Projects:</span>
+          <ul class="dashed">
+            <li>GIS Application requested by Directorate General of Land Transportation Indonesia</li>
+            <li>Patient Medical Data Storing Application requested by Medical Doctor Network Indonesia</li>
+            <li>Harbor Reporting Application requested by Directorate of Traffic and Sea Transportation Indonesia</li>
+            <li>Transportation Dashboard &amp; Report Application requested by Ministry of Transportation Indonesia</li>
+          </ul>
         </div>
-      </transition>  
+      </div>
     </div>
   </transition>
 </template>
@@ -151,49 +135,34 @@ export default {
 
 
 <style scoped>
-h2,
-h3,
-h4 {
+h1, h2 {
+  font-weight: normal;
+}
+
+h1 {
   margin: 0;
 }
 
 h2 {
-  font-weight: lighter;
-}
-
-h3 {
-  font-weight: bold;
+  margin-top: 0;
 }
 
 ul.dashed {
   margin-top: 0;
-  padding-left: 0.7em;
+  padding: 0;
   list-style-type: none;
 }
 
 ul.dashed > li {
-  text-indent: -0.7em;
-}
-
-ul.dashed > li:before {
-  content: "• ";
-  color: gray;
+  padding-bottom: 20px;
+  text-align: justify;
 }
 
 .content-wrapper {
-  padding: 0 20px;
+  padding: 20px 20px;
   text-align: left;
-}
-
-.boxed {
   background-color: white;
-  padding: 20px 30px;
-  margin-bottom: 10px;
-}
-
-.boxed-inline {
-  display: inline-block;
-  padding: 10px 20px;
+  color: black;
 }
 
 .float-right {
@@ -204,6 +173,10 @@ ul.dashed > li:before {
 .section-title {
   font-size: 1.8em;
   font-weight: bold;
+}
+
+.right-content {
+  margin-bottom: 10px;
 }
 
 .company-logo {
